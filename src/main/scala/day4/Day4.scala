910e9bf145d1c2c38803e9c7bcb9e0e606a8d1e1
s10a137g <s10a137g@gmail.com>
2021-02-14 18:48:36 +0900

    fix(Day4): Fix question1

--- src/main/scala/day4/Day4.scala
+++ src/main/scala/day4/Day4.scala
@@ -12,18 +12,22 @@ object Day4 {
     println(question1(list))
   }
   def question1(list :Seq[String]): Int = {
-    var seq1: Seq[String] = Seq()
-    var seq2: Seq[String] = Seq()
-    for (row <- list) {
-      if (!row.matches("")) {
-        seq2 :+= row
-      } else {
-        seq1 :+= seq2.foldLeft("")((v1, v2) => v1.concat(" " + v2))
-        seq2 = Seq()
-      }
+    val f: String => Boolean = v => !v.matches("")
+    val splitList = split(list, f)
+    val a = splitList.map(v => {
+      val target = v.foldLeft("")((v1, v2) => v1 + " " + v2).trim
+      target.split(' ').map { v => {
+        (v.split(":")(0), v.split(":")(1))
+      }}.toMap
+    })
+    a.count(isValid)
+  }
+  def split(list: Seq[String], f: String => Boolean): Seq[Seq[String]] = {
+    val (a, b) = list.span(v => f(v))
+    b.isEmpty match {
+      case true  => Seq(a)
+      case false => Seq(a) ++ split(b.tail, f)
     }
-    convertListToMap(seq1).map(v => isValid(v)).count(_ == true)
-
   }
   def convertListToMap(list: Seq[String]): Seq[Map[String, String]] = {
     list.map(v1=> {
@@ -33,5 +37,5 @@ object Day4 {
     })
   }
 
-  def isValid(target: Map[String, String]): Boolean = target.size == 8 || (target.size == 7 && target.contains("cid"))
+  def isValid(target: Map[String, String]): Boolean = target.size == 8 || (target.size == 7 && !target.contains("cid"))
 }
